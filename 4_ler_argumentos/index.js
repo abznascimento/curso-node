5afb23fe5535237d09f3857743db1cdc7d2ffd31
abznascimento <ana.beatriznas@outlook.com>
2022-06-14 12:04:44 -0300

    add arquivos core modules, ler argumentos, intalando minimist

---
+++ 4_ler_argumentos/index.js
@@ -0,0 +1,13 @@
+// nome
+
+const { clear } = require("console")
+
+console.log(process.argv)
+
+const args = process.argv.slice(2)
+
+console.log(args)
+
+const nome = args[0].slice("=")[1]
+
+console.log(nome)
